addb7785cb4032247a301af495a58449a9af3ac6
StasLakh <doctorbyron05@gmail.com>
2016-10-23 11:33:06 -0700

    ishop

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   has_one :account
   has_one :cart, through: :account
   
-  validates :name, presence: true, length: {minimum: 2}, on: :create
-  validates :age, inclusion: {in: 12..100}, on: :create
-  validates :balance, numericality: {greater_than_or_equal_to => 0}, on: :create
-  validates :email, format: {with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }, allow_nil: true, allow_blank: true, on: :create
+  validates :name, presence: true, length: {minimum: 2}
+  validates :age, inclusion: {in: 12..100}
+  validates :balance, numericality: {greater_than_or_equal_to => 0}
+  validates :email, format: {with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }, allow_nil: true, allow_blank: true
 
  
   after_create :create_account_for_user
