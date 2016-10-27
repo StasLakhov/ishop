ada4a303ddf34872273ec9de2a0aeb0ea0e0af2e
StasLakh <doctorbyron05@gmail.com>
2016-10-27 02:38:08 -0700

    add carts fixes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :new, :create, :show, :destroy]
   resources :users
-  resources :carts
+  resource :cart
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
